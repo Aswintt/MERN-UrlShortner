dd2cdfb09fad205e103e41a543f45b82562fbf0a
Aswin T <98648665+Aswintt@users.noreply.github.com>
2025-01-03 04:13:31 +0530

    frontend done- basic url shortnel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [copyStatus, setCopyStatus] = useState('');
+
+  const handleSub = () => {
+    axios.post('http://localhost:3002/api/short', {originalUrl})
+    .then((res) => {
+      setShortUrl(res.data.url.shortUrl);
+      // console.log("API response", res.data.url.shortUrl );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    console.log(originalUrl)
+  }
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(`http://localhost:3002/${shortUrl}`);
+      setCopyStatus("Link copied!");
+    } catch (err) {
+      setCopyStatus("Failed to copy!");
+      console.error("Error copying to clipboard: ", err);
+    }
+
+    // Reset the status after a few seconds
+    setTimeout(() => setCopyStatus(""), 2000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <input
+      value={originalUrl}
+      onChange={(e) => setOriginalUrl(e.target.value)} type='text' name='originalUrl' id=''/>
+      <button onClick={handleSub} className='btn'>Shorten</button>
+      {
+        shortUrl && (
+          <>
+          <p>Shorten Url : </p>
+          <a href={`http://localhost:3002/${shortUrl}`} target='_blank'>http://localhost:3002/{shortUrl}</a>
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <button onClick={handleCopy} style={{ padding: "10px 20px", fontSize: "16px" }}>
+            Copy Link
+          </button>
+          {copyStatus && <p>{copyStatus}</p>}
+        </div>
+          </>
+        )
+      }
     </div>
   );
 }
